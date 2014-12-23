70c7e9046554e1fd15b87355fbee42f840745157
Caroline Orsi <caroline.orsi@gmail.com>
2014-12-23 14:52:19 -0800

    Added messaging function.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
 SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', "abcdefg")
 
@@ -17,10 +16,20 @@ def index():
 @app.route("/email", methods=["POST"])
 def email():
     """ Placeholder. """
+    to_email = request.form.get("to")
+    to_name = request.form.get("to_name")
+    from_email = request.form.get("from")
+    from_name = request.form.get("from_name")
+    subject = request.form.get("subject")
+    body = request.form.get("body")
+
+    print to_email, to_name, from_email, from_name, subject, body
+
+
     return "success"
 
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 5001))
+    PORT = int(os.environ.get("PORT", 5000))
     DEBUG = "NO_DEBUG" not in os.environ
     app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
